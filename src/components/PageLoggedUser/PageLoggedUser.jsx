a2ddb0c606c7b69acf0b48bb28b75200b71b642c
Bohdana28 <bog.olhowetzcka@gmail.com>
2024-08-14 22:27:51 +0300

    book-contacts

---
+++ src/components/PageLoggedUser/PageLoggedUser.jsx
@@ -0,0 +1,11 @@
+import css from "./PageLoggedUser.module.css";
+import { Link } from "react-router-dom";
+
+export default function PageLoggedUser() {
+    return (
+        <div className={css.container}>
+            <p className={css.text}>Hello! You are on the main page of our app. To view your contact list, go to the <Link className={css.link} to="/contacts">Contacts</Link>.</p>
+            <Link className={css.link} to="/contacts"></Link>
+        </div>
+    )
+}
